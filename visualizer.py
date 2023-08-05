f5deab498e7e6c6b9ef0dc350561e64a77548fab
Miran <mirankavinda12@gmail.com>
2023-08-05 22:59:44 +0530

    pygame_event_loop

--- visualizer.py
+++ visualizer.py
@@ -39,3 +39,29 @@ def generate_starting_list(n, min_val, max_val):
         lst.append(val)
 
     return lst
+
+# pygame event loop
+def main():
+    run = True
+    clock = pygame.time.Clock()
+
+    n = 50
+    min_val = 0
+    max_val =100
+
+    lst = generate_starting_list(n, min_val, max_val)
+    draw_info = DrawInformation(600, 400, lst)
+
+    while run:
+        clock.tick(60)
+
+        pygame.display.update()
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+
+    pygame.quit()
+
+if __name__ == "__main__":
+    main()
