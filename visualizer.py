710067d4a08af857fc2e1bd4e253591091ae30f5
Miran <mirankavinda12@gmail.com>
2023-08-05 22:49:01 +0530

    gen-starting-lst

--- visualizer.py
+++ visualizer.py
@@ -29,4 +29,13 @@ class DrawInformation:
         self.block_pixel_width = round((self.width - self.SIDE_PAD) / len(lst))
         self.block_height = round((self.height - self.TOP_PAD) / (self.max_val - self.min_val))
         self.start_x = self.SIDE_PAD // 2
-        #15:07
+        
+        # generate starting list
+def generate_starting_list(n, min_val, max_val):
+    lst = []
+
+    for _ in range(n):
+        val = random.randint(min_val, max_val)
+        lst.append(val)
+
+    return lst
